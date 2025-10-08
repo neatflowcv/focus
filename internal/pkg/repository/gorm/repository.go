fd4df6fcc3f4a4ad7db84174b537cf3dd70c40fa
biosvos <biosvos@gmail.com>
2025-10-08 19:07:04 +0900

    relation repostiory 구현

--- internal/pkg/repository/gorm/repository.go
+++ internal/pkg/repository/gorm/repository.go
@@ -2,6 +2,7 @@ package gorm
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 
@@ -11,7 +12,10 @@ import (
 	"gorm.io/gorm"
 )
 
-var _ repository.Repository = (*Repository)(nil)
+var (
+	_ repository.Repository         = (*Repository)(nil)
+	_ repository.RelationRepository = (*Repository)(nil)
+)
 
 type Repository struct {
 	db *gorm.DB
@@ -63,4 +67,76 @@ func (r *Repository) GetTask(ctx context.Context, username string, id domain.Tas
 	}
 
 	return task.ToDomain(), nil
+}
+
+func (r *Repository) CreateRelation(ctx context.Context, relation *domain.Relation) error {
+	err := gorm.G[Relation](r.db).Create(ctx, FromDomainRelation(relation))
+	if err != nil {
+		if errors.Is(err, gorm.ErrDuplicatedKey) {
+			return repository.ErrRelationAlreadyExists
+		}
+
+		return fmt.Errorf("failed to create relation: %w", err)
+	}
+
+	return nil
+}
+
+func (r *Repository) DeleteRelation(ctx context.Context, relation *domain.Relation) error {
+	affected, err := gorm.G[Relation](r.db).
+		Where(&Relation{ID: string(relation.ID())}). //nolint:exhaustruct
+		Delete(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to delete relation: %w", err)
+	}
+
+	if affected == 0 {
+		return repository.ErrRelationNotFound
+	}
+
+	return nil
+}
+
+func (r *Repository) GetRelation(ctx context.Context, id domain.RelationID) (*domain.Relation, error) {
+	relation, err := gorm.G[Relation](r.db).
+		Where(&Relation{ID: string(id)}). //nolint:exhaustruct
+		Take(ctx)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, repository.ErrRelationNotFound
+		}
+
+		return nil, fmt.Errorf("failed to get relation: %w", err)
+	}
+
+	return relation.ToDomain(), nil
+}
+
+func (r *Repository) ListChildrenRelations(ctx context.Context, id domain.RelationID) ([]*domain.Relation, error) {
+	relations, err := gorm.G[Relation](r.db).
+		Where(&Relation{ParentID: sql.NullString{String: string(id), Valid: true}}). //nolint:exhaustruct
+		Find(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list children relations: %w", err)
+	}
+
+	return ToDomainRelations(relations), nil
+}
+
+func (r *Repository) UpdateRelation(ctx context.Context, dRelataion *domain.Relation) error {
+	relation := FromDomainRelation(dRelataion)
+	relation.Version++
+
+	affected, err := gorm.G[Relation](r.db).
+		Where(&Relation{ID: string(dRelataion.ID()), Version: dRelataion.Version()}). //nolint:exhaustruct
+		Updates(ctx, *relation)
+	if err != nil {
+		return fmt.Errorf("failed to update relation: %w", err)
+	}
+
+	if affected == 0 {
+		return repository.ErrRelationBusy
+	}
+
+	return nil
 }
